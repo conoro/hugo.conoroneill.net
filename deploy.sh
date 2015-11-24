12a473fd83e37c98411405a51e75026162701fbf
Conor ONeill <cwjoneill@gmail.com>
2015-11-24 16:38:19 +0000

    fix for Git identity

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+git config --global user.email "cwjoneill@gmail.com"
+git config --global user.name "Conor O'Neill"
+
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
