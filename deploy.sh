c7ebd315ec2a492d1e222e766697e5d4f2bed118
Conor ONeill <cwjoneill@gmail.com>
2015-12-06 20:34:53 +0000

    another go at travis

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 git config --global user.email "cwjoneill@gmail.com"
 git config --global user.name "Conor O'Neill"
 
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+  
 sed -i 's/git@github.com:/https:\/\/${CONOR}@github.com\//' .gitmodules
 sed -i 's/git@github.com:/https:\/\/${CONOR}@github.com\//' .git/config
   
