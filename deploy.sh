ba929427d3185762c1b4988716f3562eafe01b2c
Conor O'Neill <cwjoneill@gmail.com>
2015-12-27 14:53:22 +0000

    is non sub module working 2

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,13 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 git config --global user.email "cwjoneill@gmail.com"
 git config --global user.name "Conor O'Neill"
 
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
+
+git clone https://github.com/conoro/conoro.github.io.git
+cp -R public/* ${REPO}
+cd ${REPO}
+git remote
+git add -A :/
+git commit -a -m "latest via travis"
+git push "https://${TRAVIS_SECURE_TOKEN_NAME}@${GH_REPO}" master > /dev/null 2>&1
